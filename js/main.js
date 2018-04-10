8dbda5f71e96c197d3d7cd9b8ffba10ba843087c
kleyton Ramos <kleytonmatosramos@gmail.com>
2018-04-10 02:38:23 -0300

    add function calculate imc, get peso, get altura

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 
+// selecionar todos os elementos da classe paciente
 var vPacientes = document.querySelectorAll(".paciente");
 
+getValues();
 
-	for (i = 0; i < 4; i++) {
+function getValues(){
+	
+	for (i = 0; i < vPacientes.length; i++) {
 
 		tdPeso = vPacientes[i].querySelector(".info-peso");
 		peso = tdPeso.textContent;
@@ -14,33 +18,87 @@ var vPacientes = document.querySelectorAll(".paciente");
 		gordura = tdGordura.textContent;
 
 		tdImc = vPacientes[i].querySelector(".info-imc");
-		imc = peso / (altura*altura);
+		imc = peso/(altura*altura)
+		imc = parseFloat(imc.toFixed(2));
 
-    tdResult = vPacientes[i].querySelector(".info-result")
-
+    tdResult = vPacientes[i].querySelector(".info-result");
+    
 		if (peso <= 1 || peso > 300) {
+      
       tdImc.textContent = "Peso Inválido";
-    }
-    else if (altura < 1 || altura > 3.00) {
+      tdResult.style.backgroundColor = "red";
+    
+    }else if (altura < 1 || altura > 3.00) {
+
 			tdImc.textContent = "Altura Inválida"
-    }else tdImc.textContent = imc;
+			tdResult.style.backgroundColor = "red";
+
+    }else{
+    	tdImc.textContent = imc;
+    }
 
 		if(tdImc.textContent == "Peso Inválido"  || tdImc.textContent == "Altura Inválida" ){
+    
       tdResult.style.backgroundColor = "blue"; 
       tdResult.textContent = "Indefinido";
-		}else {
+		
+		}else{
+		 	
 		 	if (imc < 18.5) {
+
          tdResult.style.backgroundColor = "red";
          tdResult.textContent = "Abaixo do peso";
+      }
 
-      }else if (imc >= 18.5 && imc < 25) {
+      else if (imc >= 18.5 && imc < 25) {
         tdResult.style.backgroundColor = "green";
-		  	 tdResult.textContent = "Peso normal";
-		 	}else if (imc >= 25 && imc < 30) {
+		  	tdResult.textContent = "Peso normal";
+		 	}
+
+		 	else if (imc >= 25 && imc < 30) {
         tdResult.style.backgroundColor = "orange";
-		 		 tdResult.textContent = "Acima do peso";
-		 	}else tdResult.textContent = "Obeso";
+		 		tdResult.textContent = "Acima do peso";
+		 	}
+
+		 	else{
+		 	  tdResult.style.backgroundColor = "red";
+		 	  tdResult.textContent = "Obeso";
+		 	}
     }
+	}
 }
 
-  
+
+function getRowPeso(element) {
+	p = element.closest('tr').rowIndex -1;
+	//alert(p);
+	value = prompt("Informe um valor para peso: ");
+	if (value == null) {
+		 value = vPacientes[p].querySelector(".info-peso").textContent;
+	}
+	vPacientes[p].querySelector(".info-peso").innerHTML = value;
+	getValues();
+}
+
+function getRowNome(element) {
+	p = element.closest('tr').rowIndex -1;
+	//alert(p);
+	value = prompt("Informe um novo nome: ");
+	if (value == null) {
+		 value = vPacientes[p].querySelector(".info-nome").textContent;
+	}
+	vPacientes[p].querySelector(".info-nome").innerHTML = value;
+	getValues();
+}
+
+function getRowAltura(element) {
+	p = element.closest('tr').rowIndex -1;
+	//alert(p);
+	value = prompt("Informe uma nova altura: ");
+	if (value == null) {
+		 value = vPacientes[p].querySelector(".info-altura").textContent;
+	}
+	vPacientes[p].querySelector(".info-altura").innerHTML = value;
+	getValues();
+}
+
